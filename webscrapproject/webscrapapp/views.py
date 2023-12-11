33e46b7ead3bdd1cf42bf021c03cd6dc41b65afd
Achilles Jilson <akinjilson4@gmail.com>
2023-12-11 20:49:12 +0530

    webscrap added

---
+++ webscrapproject/webscrapapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+import requests
+from bs4 import BeautifulSoup
+from . models import Links
+# Create your views here.
+def home(request):
+    if request.method=='POST':
+        link=request.POST.get('page','')
+        urls=requests.get(link)
+        beautysoup=BeautifulSoup(urls.text,'html.parser')
+
+        for link in beautysoup.find_all('a'):
+            li_address=link.get('href')
+            li_name=link.string
+            Links.objects.create(address=li_address,stringname=li_name)
+        return redirect('/')
+    else:
+        data=Links.objects.all()
+    return render(request,'home.html',{'data':data})
+def clear(request):
+    link=Links.objects.all()
+    link.delete()
+    return redirect('/')
